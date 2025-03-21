16f143b64239975f90d7f15a0dbc8d5725120c08
rawanmohameedd <nrawana12345@gmail.com>
2025-03-21 15:10:00 +0200

    add rate limiter and helmet for extra layer of security

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from "express"
 import cors from "cors"
 import * as dotenv from "dotenv"; 
 import connectDB from "./config/db";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 import taskRoutes from "./routes/tasks.routes";
 import authRoutes from "./routes/auth.routes";
@@ -12,12 +14,15 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(helmet()) // sets secures http headers
 app.use(cors());
 app.use(express.json());
+app.use(rateLimit({windowMs: 15*60*1000, max: 100})) // 100 request for every 15 min.
 
 // Routes
 app.use("/auth", authRoutes);
 app.use("/api", taskRoutes)
+
 // Connect to database
 connectDB()
   .then(() => {
