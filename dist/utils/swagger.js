25ee285d884e1ca7b5f99c4a940ff3c9d49f4939
rawanmohameedd <nrawana12345@gmail.com>
2025-03-22 12:49:26 +0200

    .

--- dist/utils/swagger.js
+++ dist/utils/swagger.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.setupSwagger = void 0;
 const swagger_jsdoc_1 = __importDefault(require("swagger-jsdoc"));
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
 const options = {
     definition: {
         openapi: '3.0.0',
@@ -26,49 +27,16 @@ const options = {
                 bearerAuth: [],
             }],
     },
-    apis: ['./src/routes/*.ts', './src/models/*.ts'], // Path to the API docs
+    apis: ['./dist/routes/*.js', './dist/models/*.js'], // Path to the API docs
 };
 const swaggerSpec = (0, swagger_jsdoc_1.default)(options);
 const setupSwagger = (app) => {
-    // Serve swagger.json file
+    // Swagger page
+    app.use('/api-docs', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swaggerSpec));
+    // Docs in JSON format
     app.get('/api-docs.json', (req, res) => {
         res.setHeader('Content-Type', 'application/json');
         res.send(swaggerSpec);
-    });
-    // Create a custom HTML page for Swagger UI with CDN references
-    app.get('/api-docs', (req, res) => {
-        res.send(`
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <title>To Be Noted API Documentation</title>
-          <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@latest/swagger-ui.css">
-        </head>
-        <body>
-          <div id="swagger-ui"></div>
-          <script src="https://unpkg.com/swagger-ui-dist@latest/swagger-ui-bundle.js"></script>
-          <script src="https://unpkg.com/swagger-ui-dist@latest/swagger-ui-standalone-preset.js"></script>
-          <script>
-            window.onload = function() {
-              const ui = SwaggerUIBundle({
-                url: "/api-docs.json",
-                dom_id: '#swagger-ui',
-                deepLinking: true,
-                presets: [
-                  SwaggerUIBundle.presets.apis,
-                  SwaggerUIStandalonePreset
-                ],
-                plugins: [
-                  SwaggerUIBundle.plugins.DownloadUrl
-                ],
-                layout: "StandaloneLayout"
-              });
-              window.ui = ui;
-            };
-          </script>
-        </body>
-      </html>
-    `);
     });
     console.log('Swagger docs available at /api-docs');
 };
--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -1,4 +1,5 @@
 import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 import { Express } from 'express';
 
 const options: swaggerJSDoc.Options = {
@@ -22,53 +23,20 @@ const options: swaggerJSDoc.Options = {
       bearerAuth: [],
     }],
   },
-  apis: ['./src/routes/*.ts', './src/models/*.ts'], // Path to the API docs
+  apis: ['./dist/routes/*.js', './dist/models/*.js'], // Path to the API docs
 };
 
 const swaggerSpec = swaggerJSDoc(options);
 
 export const setupSwagger = (app: Express) => {
-  // Serve swagger.json file
+  // Swagger page
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+  // Docs in JSON format
   app.get('/api-docs.json', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.send(swaggerSpec);
   });
 
-  // Create a custom HTML page for Swagger UI with CDN references
-  app.get('/api-docs', (req, res) => {
-    res.send(`
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <title>To Be Noted API Documentation</title>
-          <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@latest/swagger-ui.css">
-        </head>
-        <body>
-          <div id="swagger-ui"></div>
-          <script src="https://unpkg.com/swagger-ui-dist@latest/swagger-ui-bundle.js"></script>
-          <script src="https://unpkg.com/swagger-ui-dist@latest/swagger-ui-standalone-preset.js"></script>
-          <script>
-            window.onload = function() {
-              const ui = SwaggerUIBundle({
-                url: "/api-docs.json",
-                dom_id: '#swagger-ui',
-                deepLinking: true,
-                presets: [
-                  SwaggerUIBundle.presets.apis,
-                  SwaggerUIStandalonePreset
-                ],
-                plugins: [
-                  SwaggerUIBundle.plugins.DownloadUrl
-                ],
-                layout: "StandaloneLayout"
-              });
-              window.ui = ui;
-            };
-          </script>
-        </body>
-      </html>
-    `);
-  });
-  
   console.log('Swagger docs available at /api-docs');
 };
